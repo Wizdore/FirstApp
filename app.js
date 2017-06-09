d380d4b2fed5cfa54f1db4d34b13f3cd59456aec
nocmnt <shada.kaalo@gmail.com>
2017-06-10 02:16:43 +0600

    updated repo and openshift ip and port

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
-app.listen(3000);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
 app.get('/', function(req, res){
@@ -10,4 +12,8 @@ app.get('/', function(req, res){
 
 app.get('/name/:name', function(req, res){
   res.render('profile', {name: req.params.name});
+});
+
+app.listen(server_port, server_ip_adress, function(){
+  console.log("Listening on " + server_ip_adress + ", on port " + server_port);
 });

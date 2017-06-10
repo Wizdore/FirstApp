0797b7096d1bf50bd836a4b7a40f8b7f318c2438
nocmnt <shada.kaalo@gmail.com>
2017-06-10 13:25:19 +0600

    Updated port and IP for openshift

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 app.get('/', function(req, res){
@@ -15,5 +15,5 @@ app.get('/name/:name', function(req, res){
 });
 
 app.listen(server_port, server_ip_adress, function(){
-  console.log("Listening on " + server_ip_adress + ", on port " + server_port);
+  console.log("Ok, Pinik,  Listening on " + server_ip_adress + ", on port " + server_port);
 });

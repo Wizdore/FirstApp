b56bd6a6ba3fbd9ef8d61075c1d18fde638cb2e2
nocmnt <shada.kaalo@gmail.com>
2017-06-11 13:53:30 +0600

    Resume added

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var app = express();
 app.set('view engine', 'ejs');
 
 var server_port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+app.use('/fonts', express.static('fonts'));
+app.use('/images', express.static('images'));
+app.use('/javascript', express.static('javascript'));
+app.use('/stylesheets', express.static('stylesheets'));
+app.use('/toggle', express.static('toggle'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index-normal.html');
 });
 
 app.get('/name/:name', function(req, res){

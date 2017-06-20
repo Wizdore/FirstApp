c36f6fed75d8de44dfc7a12d5a1d82d4e2dddb68
nocmnt <shada.kaalo@gmail.com>
2017-06-20 14:59:01 +0600

    Final state. will work on it later

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 
 
 var server_port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' || '0.0.0.0';
 
 
 

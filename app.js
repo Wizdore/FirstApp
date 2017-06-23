2724dae4725501b510e25055df0fc68dec0bdd2c
nocmnt <shada.kaalo@gmail.com>
2017-06-24 00:42:16 +0600

    The Bot project starts

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var app = express();
-kote = require('kote-api');
 app.set('view engine', 'ejs');
-
 
 var server_port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' || '0.0.0.0';
 
-
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index-normal.html');
-// });
 
 app.get('/:name', function(req, res){
   res.render('index', {name: req.params.name});
@@ -21,6 +14,3 @@ app.listen(server_port, server_ip_adress, function(){
   console.log("Ok, Pinik,  Listening on " + server_ip_adress + ", on port " + server_port);
 });
 
-kote.eduro().then(quote => {
-    console.log(quote);
-});
